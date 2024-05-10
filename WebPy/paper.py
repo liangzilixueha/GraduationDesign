3638e24f2fd240ddafb90da79c55fc1cd9fa5a12
量子 <85008631+liangzilixueha@users.noreply.github.com>
2024-05-10 08:57:48 +0000

    完成pdf转图片

--- WebPy/paper.py
+++ WebPy/paper.py
@@ -2,7 +2,7 @@ import os
 from flask import Blueprint, jsonify, redirect, render_template, request, send_file, url_for
 from PIL import Image
 import pytesseract
-import fitz
+import pymupdf
 
 paper=Blueprint('paper',__name__)
 
@@ -14,7 +14,7 @@ def index():
 def red():
     return render_template('mark.html')
 
-@paper.route('/paper/upload', methods=['POST'])
+@paper.route('/upload', methods=['POST'])
 def upload_file():
     if 'image' not in request.files:
         return redirect(request.url)
@@ -30,16 +30,16 @@ def upload_file():
         text = pytesseract.image_to_string(Image.open(filepath),lang='chi_sim')
         return text
     
-@paper.route('/paper/uploadpdf', methods=['POST'])
+@paper.route('/uploadpdf', methods=['POST'])
 def upload_pdf():
     # 获取前端上传的文件
     file = request.files['file']
     # 保存文件
     file.save('uploads/' + file.filename)
     # 将pdf的第一页转换为图片
-    pdf_document = fitz.open('uploads/' + file.filename)
+    pdf_document = pymupdf.open('uploads/' + file.filename)
     first_page = pdf_document[0]
-    pix = first_page.get_pixmap(matrix=fitz.Matrix(2, 2))
+    pix = first_page.get_pixmap(matrix=pymupdf.Matrix(2, 2))
     image_path = 'uploads/first_page.png'
     pix.save(image_path)  # 保存图片
     pdf_document.close()  # 关闭PDF
